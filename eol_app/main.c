db11ab4288537b2c4cee4c2e184f8dbb26cd1aac
John Savill <jsavill96@gmail.com>
2025-10-03 18:14:10 +0100

    update for home build, adding executables to .gitignore

--- eol_app/main.c
+++ eol_app/main.c
@@ -5,7 +5,7 @@ int main(int argc, char **argv) {
     int status;
     AppData app_data = {0};
     
-    app = gtk_application_new("eol_tester.alpha", G_APPLICATION_FLAGS_NONE); //G_APPLICATION_FLAGS_NONE is depr
+    app = gtk_application_new("eol_tester.alpha", G_APPLICATION_DEFAULT_FLAGS); //G_APPLICATION_FLAGS_NONE is depr
     g_signal_connect(app, "activate", G_CALLBACK(activate_application), &app_data);
     
     status = g_application_run(G_APPLICATION(app), argc, argv);
